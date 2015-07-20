48628a236a48e1200acd1ace396d8a15633e0e6e
Nico Schlumprecht <snico1995@gmail.com>
2015-07-19 19:47:01 -0500

    added sha1 function, alternate decodebase32

--- Authenticator/Utils.m
+++ Authenticator/Utils.m
@@ -8,6 +8,7 @@
 
 #import "Utils.h"
 #import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation Utils
 
@@ -23,6 +24,31 @@
     return result;
 }
 
++(long long)decodeBase32_2:(const char*)s{
+    unsigned int len = strlen(s) - 1;
+    long long result = 0;
+    char start_ch = 0, ch;
+    while(*s != '\0') {
+        ch = *s;
+        if (ch >= 'a') {
+            start_ch = 'a' - 10;
+        } else if (ch >= 'A') {
+            start_ch = 'A' - 10;
+        } else {
+            start_ch = '0';
+        }
+        
+        if(len >= 0)
+            result += (ch - start_ch) * pow(32, len);
+        else
+            result += (ch - start_ch);
+        ++s;
+        --len;
+    }
+    
+    return result;
+}
+
 +(int) charVal:(char)c{
     if(c <= '9' && c >= '0'){
         return c - '0';
@@ -31,6 +57,20 @@
     } else {
         return -1;
     }
+}
+
++(NSString*) sha1:(NSString*)input {
+    NSData *data = [input dataUsingEncoding:NSUTF32StringEncoding];
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, digest);
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
 }
 
 @end
